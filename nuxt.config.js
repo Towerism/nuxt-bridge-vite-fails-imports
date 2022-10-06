d00e756b7e2802bca81256d0321ddb20f0957bca
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2022-10-06 11:53:53 -0500

    Disable nitro and enable vite

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,8 @@ import { defineNuxtConfig } from '@nuxt/bridge'
 export default defineNuxtConfig({
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
-  router: {
-    base: "/ui_template_sub/"
+  bridge: {
+    nitro: false,
+    vite: true
   }
 })
